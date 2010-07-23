c0deeb187a09e04a33c0721f9d08555abc6fa8c7
David Kelso <david@kelso.id.au>
2010-07-22 22:27:27 -0700

    v1

---
+++ README.markdown
@@ -0,0 +1,51 @@
+# css3 lightbox
+
+## About
+Why does the world need another lightbox?
+
+* It doesn't take 100k of JS to show a div
+* You shouldn't need 18 different images and 8 css files placed in a hardcoded 
+directory just to get a rounded corner
+* I know how to use click handlers. I don't need crazy tricks overriding href attributes
+
+Here's my small-as-possible completely image-less solution. Complete with CSS 
+based spinner. It only needs a single script tag. Compressed version weighs in 
+at a mighty 2.8k. Don't have a browser that can support rounded corners? 
+TOUGH. SUCK IT UP.
+
+## Requirements
+jQuery 1.4+ - I'm using the new jquery object creation syntax, so you have to 
+upgrade from 1.3
+
+## Demo
+For usage examples, check out a demo page here:
+http://zaius.github.com/css3-lightbox
+
+## Fallback
+The lightbox uses css 3 declarations for the rounded corners and box shadows. 
+This means you get a nice fallback on browsers that don't support it. In that
+case, you will see:
+
+* the spinner is made up of squares instead of circles
+* the close box is a square instead of a circle
+* there is no glow effect on the modal window
+* the modal window isn't rounded
+
+It surprisingly still looks acceptable
+
+## Changes
+I didn't implement too much more than the basics, because I have no idea if 
+people actually want this. If you want specific changes add a bug or email me,
+and I'll see what I can do.
+
+Even better, patch the code and send me a pull request!
+
+## Issues
+* Haven't event tried to see if it works in IE6. It won't.
+* Doesn't handle browser resizing.
+
+## License
+This code is licensed under the ISC license which lets you do pretty much
+whatever you want with it. Please see LICENSE for the full text.
+
+Copyright (c) 2010 David Kelso - david at kelso dot id dot au
---
+++ jquery.css3lightbox.js
@@ -0,0 +1,165 @@
+/*
+ * jQuery css3 lightbox
+ * http://github.com/zaius/css3-lightbox
+ * David Kelso 2010, david at kelso dot id dot au
+ */
+
+(function ($) {
+  var default_options = {
+    type: 'text',
+    width: 800,
+    height: 600,
+    spinner: {
+      speed: 100,
+      segments: 8
+    },
+    css: { 
+      shadow: {
+        'background-color': 'black',
+        'opacity': 0.8,
+        'position': 'fixed',
+        'left': 0, 'top': 0, 'bottom': 0, 'right': 0,
+        'z-index': 1000
+      },
+      modal: {
+        'background-color': 'black',
+        'color': 'white',
+        'padding': 10,
+        'position': 'fixed',
+        'z-index': 1001,
+        'border': '5px solid white',
+        'border-radius': 10, '-moz-border-radius': 10, '-webkit-border-radius': 10,
+        'box-shadow': '0 0 10px #fff', '-moz-box-shadow': '0 0 10px #fff', '-webkit-box-shadow': '0 0 10px #fff'
+      },
+      close_button: {
+        'cursor': 'pointer',
+        'position': 'absolute',
+        'top': -10, 'right': -10,
+        'width': 20, 'height': 18,
+        'border-radius': 20, '-moz-border-radius': 20, '-webkit-border-radius': 20,
+        'box-shadow': '5px 5px 5px #000', '-moz-box-shadow': '5px 5px 5px #000', '-webkit-box-shadow': '5px 5px 5px #000',
+        'border': '2px solid white',
+        'background': 'black',
+        'color': 'white',
+        'font-family': 'sans-serif',
+        'font-size': 14,
+        'font-weight': 'bold',
+        'padding': '2px 0 0 0px',
+        'margin': 0,
+        'text-align': 'center'
+      },
+      spinner: { 
+        'width': 200, 'height': 200,
+        'position': 'absolute'
+      },
+      spinner_ball: {
+        'position': 'absolute',
+        'width': 5,
+        'height': 5,
+        'background-color': 'white',
+        'border-radius': 5, '-moz-border-radius': 5, '-webkit-border-radius': 5,
+        'box-shadow': '0 0 2px #000', '-moz-box-shadow': '0 0 2px #000', '-webkit-box-shadow': '0 0 2px #000'
+      }
+    }
+  };
+
+  var set_dynamic_css = function(options) {
+    var dynamic_css = {
+      modal: {
+        'width': options.width, 
+        'height': options.height,
+        'left': ($(window).width() - options.width) / 2,
+        'top': ($(window).height() - options.height) / 2
+      },
+      spinner: {
+        'left': (options.width - options.css.spinner.width) / 2,
+        'top': (options.height - options.css.spinner.height) / 2
+      }
+    };
+
+    $.extend(true, options.css, dynamic_css);
+  };
+
+
+  var show = function(options) {
+    options = $.extend(true, {}, default_options, options);
+    set_dynamic_css(options);
+
+    var shadow = $('<div />', { css: options.css.shadow});
+    var modal = $('<div />', { css: options.css.modal});
+    var spinner = $('<div />', { css: options.css.spinner});
+    var close = $('<div />', { text: 'X', css: options.css.close_button});
+
+    // Spinner setup
+    var spinner_balls = [];
+    var size = Math.min(options.css.spinner.width, options.css.spinner.height);
+    for (var i = 0; i < options.spinner.segments; i++) {
+      options.css.spinner_ball.top = size / 2 + 25 * Math.sin(Math.PI * 2 * i / options.spinner.segments);
+      options.css.spinner_ball.left = size / 2 + 25 * Math.cos(Math.PI * 2 * i / options.spinner.segments);
+      spinner_balls.push($('<div />', { css: options.css.spinner_ball}));
+    }
+    $.each(spinner_balls, function() { spinner.append(this); });
+
+    var spinning = true;
+    var fade = function(i) {
+      if (!spinning) return;
+      spinner_balls[i].stop(true, true).fadeTo(0, 1).
+        fadeOut(options.spinner.segments * options.spinner.speed);
+
+      setTimeout(function() {
+        fade((i + 1) % options.spinner.segments)
+      }, options.spinner.speed);
+    };
+    
+    var start_spinner = function() { modal.append(spinner); fade(0); }
+    var stop_spinner = function() { spinner.remove(); spinning = false; }
+
+
+    // Would probably be nice to do some fading here. Couldn't be bothered.
+    var packup = function() { 
+      modal.remove();
+      shadow.remove();
+    }
+
+    shadow.click(packup);
+    close.click(packup);
+
+    modal.append(close);
+    $('body').append(shadow).append(modal);
+
+    if (options.type == 'text') {
+      modal.append($('<div />', { text: options.text }));
+    }
+    else if (options.type == 'div') {
+      var el = options.element.clone();
+      modal.append(el);
+      el.show();
+    }
+    else if (options.type == 'iframe') {
+      start_spinner();
+      var iframe = $('<iframe />', {
+        src: options.url,
+        css: {'width': 0, 'height': 0, 'border': 'none'},
+        load: function() { 
+          stop_spinner();
+          iframe.width('100%').height('100%');
+        }
+      });
+      modal.append(iframe);
+    } else if (options.type == 'ajax') {
+      start_spinner();
+      $.get(options.url, function(data) {
+        stop_spinner();
+        var content = $('<div />', {
+          html: data,
+          css: { 'overflow-y': 'scroll', 'height': options.height}
+        });
+        modal.append(content);
+      });
+    } else {
+      throw('unsupported lightbox type');
+    }
+  };
+
+  $.extend({ lightbox: show });
+})(jQuery);
